2521e66167dbdffd30e0acfda91e1b8b211af40a
telcaslor <thatbytc@gmail.com>
2025-09-09 09:29:36 +0200

    Tercer commit con los cambios del procedimiento start en la app

--- src/main/java/es/telmocas/App.java
+++ src/main/java/es/telmocas/App.java
@@ -2,10 +2,13 @@ package es.telmocas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.control.Button;
 import java.io.IOException;
 
 /**
@@ -15,21 +18,31 @@ public class App extends Application {
 
     private static Scene scene;
 
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        scene = new Scene(loadFXML("primary"), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) {
+        stage.setTitle("Stage with a Sized Scene");
+        Group root = new Group(new Button("Hello"));
+        Scene scene = new Scene(root, 300, 100);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+//
+//    static void setRoot(String fxml) throws IOException {
+//        scene.setRoot(loadFXML(fxml));
+//    }
+//
+//    private static Parent loadFXML(String fxml) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+//        return fxmlLoader.load();
+//    }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module es.telmocas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens es.telmocas to javafx.fxml;
     exports es.telmocas;
